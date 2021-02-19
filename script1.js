efff30fedebea066325a8f9baed83f3b0f484ff1
s <myae3080@naver.com>
2021-02-19 21:28:01 +0900

    shadow dom test

--- script1.js
+++ script1.js
@@ -1,4 +1,15 @@
-let pl = document.getElementById('pl');
-pl.addEventListener('click', () => {
-    window.location.href = 'jsPage.html';
-});
+// let pl = document.getElementById('pl');
+// pl.addEventListener('click', () => {
+//     window.location.href = 'jsPage.html';
+// });
+
+/*
+    * shadow dom을 사용하는 이유
+    - css가 shadowRoot 내로 한정지어진다. -> style encapsulation
+*/
+window.onload = () => {
+    const targetElement = document.getElementById('elem');
+
+    targetElement.attachShadow({mode: 'open'});
+    targetElement.shadowRoot.innerHTML = `<h1>Shadow Dom</h1>`;
+}
